12e5f0f096cae511e64e17d09d282162e4001985
Lal Bahadur BC <lalbahadurbc2054@gmail.com>
2023-04-12 03:09:04 +0545

    chore(mobile): mobile responsive homepage

--- srcript/script.js
+++ srcript/script.js
@@ -5,6 +5,7 @@ $(".bestSellers__slider").slick({
   speed: 300,
   slidesToShow: 4,
   slidesToScroll: 4,
+  // mobileFirst: true,
   responsive: [
     {
       breakpoint: 1024,
@@ -16,25 +17,21 @@ $(".bestSellers__slider").slick({
       },
     },
     {
-      breakpoint: 600,
+      breakpoint: 768,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2,
       },
     },
     {
-      breakpoint: 480,
+      breakpoint: 578,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
       },
     },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ],
 });
-
 $(".clientsReview__slider").slick({
   dots: false,
   arrows: true,
@@ -42,9 +39,10 @@ $(".clientsReview__slider").slick({
   speed: 300,
   slidesToShow: 4,
   slidesToScroll: 4,
+  autoPlay: true,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 992,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
@@ -53,17 +51,21 @@ $(".clientsReview__slider").slick({
       },
     },
     {
-      breakpoint: 600,
+      breakpoint: 768,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2,
+        dots: true,
+        arrows: false,
       },
     },
     {
-      breakpoint: 480,
+      breakpoint: 568,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
+        dots: true,
+        arrows: false,
       },
     },
     // You can unslick at a given breakpoint now by adding:
@@ -79,4 +81,40 @@ $(".footerClientReviews__slider").slick({
   speed: 300,
   slidesToShow: 1,
   slidesToScroll: 1,
+  autoPlay:true,
+  responsive: [
+    {
+      breakpoint: 568,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+      },
+    },
+  ],
+});
+
+// mobile nav dropdown
+let toggleDropdownIcon = true;
+$(".mobile__nav .navLinks__list .nav__dropdown").click(function () {
+  $(this).find(".vertical__dropdownMenu").slideToggle();
+  $(this).find(".open__btn i").toggleClass("bi-chevron-up");
+});
+
+//search input popup
+
+$("#search__icon").click(function () {
+  $(".search__toggle__content").slideUp();
+  $("#mobile__popup__wrapper").slideDown();
+});
+$("#close__mobile__popup").click(function () {
+  $(".search__toggle__content").slideDown();
+  $("#mobile__popup__wrapper").slideUp();
+});
+
+//footer accordion
+$(".top__footer .footer__content .title").click(function () {
+  $(this).closest(".footer__content").find(".footerLinks__list").slideToggle();
+  $(this).find("i").toggleClass("rotate__180");
 });
